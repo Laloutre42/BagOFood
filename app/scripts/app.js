342856baab4a02e2adf6268311581e4588e549c3
Arnaud Zdziobeck <Laloutre42@gmail.com>
2015-04-26 10:24:55 +0200

    Init Yeoman angular project

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,36 +2,34 @@
 
 /**
  * @ngdoc overview
- * @name bagOfoodApp
+ * @name bagOFoodApp
  * @description
- * # bagOfoodApp
+ * # bagOFoodApp
  *
  * Main module of the application.
  */
-angular
-  .module('bagOfoodApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+angular.module('bagOFoodApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'elasticsearch',
+        'bagOFoodControllers',
+        'bagOFoodServices'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'BagOFoodCtrl'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
