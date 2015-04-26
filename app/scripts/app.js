b46a95068ce5bbc3302905a0d0202c8055c08c90
Arnaud Zdziobeck <Laloutre42@gmail.com>
2015-04-26 18:55:23 +0200

    add pagination

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,27 +9,28 @@
  * Main module of the application.
  */
 angular.module('bagOFoodApp', [
-        'ngAnimate',
-        'ngCookies',
-        'ngResource',
-        'ngRoute',
-        'ngSanitize',
-        'ngTouch',
-        'elasticsearch',
-        'bagOFoodControllers',
-        'bagOFoodServices'
-    ])
-    .config(function ($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'views/main.html',
-                controller: 'BagOFoodCtrl'
-            })
-            .when('/about', {
-                templateUrl: 'views/about.html',
-                controller: 'AboutCtrl'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    });
+  'ngAnimate',
+  'ngCookies',
+  'ngResource',
+  'ngRoute',
+  'ngSanitize',
+  'ngTouch',
+  'elasticsearch',
+  'ui.bootstrap',
+  'bagOFoodControllers',
+  'bagOFoodServices'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'BagOFoodCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,30 +12,53 @@ var bagOFoodControllers = angular.module('bagOFoodControllers', []);
  */
 bagOFoodControllers.controller('BagOFoodCtrl', function ($scope, elasticSearchService, esFactory) {
 
-    $scope.searchProductCiqual2012 = function () {
 
-        $scope.productCiqual2012Results = [];
+  // Pagination var
+  $scope.itemsPerPage = 10;
+  $scope.maxSize = 10;
+  $scope.productCiqual2012Results = [];
+  $scope.filteredProductCiqual2012Results = [];
 
-        elasticSearchService.search({
-                index: 'bagofood',
-                type: 'ciqual2012',
-                fields: ['ORIGGPFR', 'ORIGFDNM'],
-                q: 'ORIGFDNM:*' + $scope.productCiqual2012 + '*'
+  /**
+   * Watch pagination
+   */
+  $scope.$watch("currentPage", function () {
+    var begin = (($scope.currentPage - 1) * $scope.itemsPerPage);
+    var end = begin + $scope.itemsPerPage;
+    $scope.filteredProductCiqual2012Results = $scope.productCiqual2012Results.slice(begin, end);
+  });
 
-            })
-            .then(function (resp) {
-                resp.hits.hits.forEach(function (hit) {
-                    $scope.productCiqual2012Results.push(hit.fields);
-                });
-                $scope.error = null;
-            })
-            .catch(function (err) {
-                $scope.error = err;
-                // if the err is a NoConnections error, then the elasticSearchService was not able to connect to elasticsearch. In that case, create a more detailed error message
-                if (err instanceof esFactory.errors.NoConnections) {
-                    $scope.error = new Error('Unable to connect to elasticsearch. Make sure that it is running and listening at http://localhost:9200');
-                }
-            });
+  /**
+   * Search product in Ciqual 2012 database
+   */
+  $scope.searchProductCiqual2012 = function () {
 
-    };
+    $scope.productCiqual2012Results = [];
+    $scope.filteredProductCiqual2012Results = [];
+
+    elasticSearchService.search({
+      index: 'bagofood',
+      type: 'ciqual2012',
+      size: '1000',
+      fields: ['ORIGGPFR', 'ORIGFDNM'],
+      q: 'ORIGFDNM:*' + $scope.productCiqual2012 + '*'
+
+    })
+      .then(function (resp) {
+        resp.hits.hits.forEach(function (hit) {
+          $scope.productCiqual2012Results.push(hit.fields);
+        });
+        $scope.error = null;
+        // New search, start from 1
+        $scope.currentPage = 1;
+      })
+      .catch(function (err) {
+        $scope.error = err;
+        // if the err is a NoConnections error, then the elasticSearchService was not able to connect to elasticsearch. In that case, create a more detailed error message
+        if (err instanceof esFactory.errors.NoConnections) {
+          $scope.error = new Error('Unable to connect to elasticsearch. Make sure that it is running and listening at http://localhost:9200');
+        }
+      });
+
+  };
 });
