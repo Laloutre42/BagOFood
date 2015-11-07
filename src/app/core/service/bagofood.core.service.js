7fc89850e54fa211f8f2a9bc9a1237b95cf17531
Arnaud Zdziobeck <Laloutre42@gmail.com>
2015-11-07 08:14:12 +0100

    Remove custom previous state and depends on ui-router extra

--- src/app/core/service/bagofood.core.service.js
+++ src/app/core/service/bagofood.core.service.js
@@ -5,7 +5,6 @@
 	  'bagofood.core.service.foodlist',
 	  'bagofood.core.service.product',
 	  'bagofood.core.service.product.modal.details',
-	  'bagofood.core.service.previousState',
 	  'bagofood.core.service.items',
 	  'bagofood.core.service.authentication',
     'bagofood.core.service.authenticationInterceptor',
